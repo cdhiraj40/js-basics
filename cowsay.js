c7190d66349e6712ad227ca64ae51d3041ad4549
Dhiraj Chauhan <Chauhandhiraj40@gmail.com>
2022-11-23 06:55:43 +0000

    Create conditions.js and cowsay.js

---
+++ cowsay.js
@@ -0,0 +1,12 @@
+const cowsay = require("cowsay")
+
+const Quote = require('inspirational-quotes')
+
+const text = Quote.getQuote().text
+console.log(
+    cowsay.say({
+        text : text,
+        e : "xx",
+        T : "U "
+})
+);
